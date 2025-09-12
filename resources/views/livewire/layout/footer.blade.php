1b9460a61e5b20a5c8deb4d165b6612ec6465318
Ivanh00 <97252781+Ivanh00@users.noreply.github.com>
2025-09-12 23:03:01 +0200

    Footer light line removed

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white dark:bg-gray-900 text-gray-700 dark:text-white border-t border-gray-200 dark:border-gray-700 mt-12">
+<footer class="bg-white dark:bg-gray-900 text-gray-700 dark:text-white border-t border-gray-200 dark:border-gray-700">
     <div class="max-w-7xl mx-auto px-4 py-12">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <!-- About MyOglasi -->
