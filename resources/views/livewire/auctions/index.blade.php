15037f0a81ba87ae6bfc3876ca60dbfd4742984e
Ivanh00 <97252781+Ivanh00@users.noreply.github.com>
2025-09-17 22:08:07 +0200

    Auctions, removed disabled buy now button before start

--- resources/views/livewire/auctions/index.blade.php
+++ resources/views/livewire/auctions/index.blade.php
@@ -506,17 +506,24 @@
                                 </div>
 
                                 <div class="space-y-2">
-                                    <a href="{{ route('auction.show', $auction) }}"
-                                        class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
-                                        <i class="fas fa-eye mr-2"></i> Pogledaj detalje
-                                    </a>
-
-                                    @if ($auction->buy_now_price)
-                                        <button disabled
-                                            class="block w-full text-center px-3 py-2 bg-gray-400 text-gray-300 rounded-lg cursor-not-allowed text-sm">
-                                            <i class="fas fa-shopping-cart mr-2"></i> Kupi odmah (počinje {{ $auction->starts_at->format('d.m') }})
-                                        </button>
-                                    @endif
+                                    @auth
+                                        @if(auth()->id() === $auction->user_id)
+                                            <a href="{{ route('listings.edit', $auction->listing) }}"
+                                                class="block w-full text-center px-3 py-2 bg-yellow-600 text-white rounded-lg hover:bg-yellow-700 transition-colors text-sm">
+                                                <i class="fas fa-gavel mr-2"></i> Uredi aukciju
+                                            </a>
+                                        @else
+                                            <a href="{{ route('auction.show', $auction) }}"
+                                                class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
+                                                <i class="fas fa-eye mr-2"></i> Pogledaj detalje
+                                            </a>
+                                        @endif
+                                    @else
+                                        <a href="{{ route('auction.show', $auction) }}"
+                                            class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
+                                            <i class="fas fa-eye mr-2"></i> Pogledaj detalje
+                                        </a>
+                                    @endauth
                                 </div>
                             </div>
                         </div>
@@ -617,17 +624,24 @@
                                         </div>
 
                                         <div class="space-y-2">
-                                            <a href="{{ route('auction.show', $auction) }}"
-                                                class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
-                                                <i class="fas fa-eye mr-2"></i> Pogledaj detalje
-                                            </a>
-
-                                            @if ($auction->buy_now_price)
-                                                <button disabled
-                                                    class="block w-full text-center px-3 py-2 bg-gray-400 text-gray-300 rounded-lg cursor-not-allowed text-sm">
-                                                    <i class="fas fa-shopping-cart mr-2"></i> Kupi odmah (počinje {{ $auction->starts_at->format('d.m') }})
-                                                </button>
-                                            @endif
+                                            @auth
+                                                @if(auth()->id() === $auction->user_id)
+                                                    <a href="{{ route('listings.edit', $auction->listing) }}"
+                                                        class="block w-full text-center px-3 py-2 bg-yellow-600 text-white rounded-lg hover:bg-yellow-700 transition-colors text-sm">
+                                                        <i class="fas fa-gavel mr-2"></i> Uredi aukciju
+                                                    </a>
+                                                @else
+                                                    <a href="{{ route('auction.show', $auction) }}"
+                                                        class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
+                                                        <i class="fas fa-eye mr-2"></i> Pogledaj detalje
+                                                    </a>
+                                                @endif
+                                            @else
+                                                <a href="{{ route('auction.show', $auction) }}"
+                                                    class="block w-full text-center px-3 py-2 bg-yellow-700 text-white rounded-lg hover:bg-yellow-800 transition-colors text-sm">
+                                                    <i class="fas fa-eye mr-2"></i> Pogledaj detalje
+                                                </a>
+                                            @endauth
                                         </div>
                                     </div>
                                 </div>
