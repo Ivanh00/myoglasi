3b1157664f69bcfa0f6f49f8d852d4515e5f063e
Ivanh00 <97252781+Ivanh00@users.noreply.github.com>
2025-09-18 17:13:27 +0200

    Removed my ratings from sidebar

--- resources/views/livewire/category-sidebar.blade.php
+++ resources/views/livewire/category-sidebar.blade.php
@@ -286,23 +286,6 @@
                     @endauth
                 </a>
 
-                <a href="{{ route('ratings.my') }}"
-                    class="flex items-center px-3 py-2 text-blue-600 dark:text-blue-400 rounded-lg hover:bg-blue-50 dark:hover:bg-gray-700 mt-2">
-                    <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
-                        </path>
-                    </svg>
-                    Moje ocene
-                    @php
-                        $totalRatings = auth()->user()->total_ratings_count ?? 0;
-                    @endphp
-                    @if ($totalRatings > 0)
-                        <span class="ml-2 bg-green-600 text-white rounded px-2 py-1 text-xs font-medium">
-                            {{ $totalRatings }}
-                        </span>
-                    @endif
-                </a>
             </div>
         @endif
     @endauth
