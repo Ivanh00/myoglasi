6de8424c2d8ab4a6538b39337fc45543f6caf0df
Ivanh00 <97252781+Ivanh00@users.noreply.github.com>
2025-10-02 00:52:00 +0200

    Translationof pagination. and make Locale SR json

--- resources/lang/sr/messages.php
+++ resources/lang/sr/messages.php
@@ -2,5 +2,12 @@
 
 return [
     'posted_ago' => 'postavljeno pre :vreme',
+
+    // Pagination
+    'Showing' => 'Prikazano',
+    'to' => 'do',
+    'of' => 'od',
+    'results' => 'rezultata',
+
     // drugi prevodi...
 ];
